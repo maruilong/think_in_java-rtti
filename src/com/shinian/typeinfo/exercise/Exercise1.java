3280bbef4e21700ac5d32e260e299726a11614bf
maruilong <774328384@qq.com>
2017-07-17 20:23:37 +0800

    练习1 完毕

---
+++ src/com/shinian/typeinfo/exercise/Exercise1.java
@@ -0,0 +1,17 @@
+package com.shinian.typeinfo.exercise;
+
+/**
+ * 
+* @ClassName: Exercise1
+* @Description: 注释ToyTest中的默认构造器
+* @author shinian
+* @date 2017年7月17日 下午8:13:07
+*
+ */
+public class Exercise1 {
+
+    /**
+     * 出现java.lang.NullPointerException ,可以推断出class.newInstance 是使用默认构造器创建的
+     */
+    
+}
